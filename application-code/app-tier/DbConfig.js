a9c11977ea29c4e0d49590f23a620f0bcdf03182
Karan S <148225109+KaranPrince@users.noreply.github.com>
2025-08-16 20:49:36 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
-module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
-});
+module.exports = {
+  host: "<RDS-ENDPOINT>",
+  user: "<DB-USERNAME>",
+  password: "<DB-PASSWORD>",
+  database: "<DB-NAME>"
+};
